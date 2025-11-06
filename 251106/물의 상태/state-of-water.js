1e4c403fe57e187ed7ce08061fb064f8d5ca3b6e
Gunwoo Kim <kenyouzzang@gmail.com>
2025-11-06 16:19:59 +0900

    Title: 물의 상태, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251106/물의 상태/state-of-water.js
@@ -0,0 +1,15 @@
+// 입력 받기 (예: Node.js 환경)
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').trim(); 
+const n = parseInt(input);
+
+// 조건문으로 상태 판별
+if (n <= 0) {
+  console.log("ice");
+  } 
+else if (n >= 100) {
+    console.log("vapor");
+    }
+else {
+    console.log("water");
+}
